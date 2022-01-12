b8f6bb43f217618adaca800a6ccd80d5f3de996a
daesoo94 <tlaeotn123@gmail.com>
2022-01-12 16:10:19 +0900

    graphql config 추가 및, 쓸모없는 데이터 제거

---
+++ strapi/config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = {
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+};
